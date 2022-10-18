9e3abdf168070dce97bc71c62eeab734b791c2ab
AyushVerma2772 <ayushhathiwan@gmail.com>
2022-10-18 07:06:06 +0530

    change1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href='/' >Meri TodoList</a>
+                    <a className="navbar-brand" href='/'>Meri TodoList</a>
                     <div>
-                        <span className='text-white fw-semibold mx-4' >Ayush</span>
+                        <span className='text-white fw-semibold mx-4'>Ayush</span>
                         <button className="mx-2 btn btn-sm btn-danger" type="submit" >Close</button>
                     </div>
                 </div>
